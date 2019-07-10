395bda6f6f70424fce2a44ebca67a98fadb00574
Christian Nguyen <christiannguyen110591@gmail.com>
2019-07-10 13:21:32 -0400

    Threading

---
+++ Week9.1-ThreadExample1/Week9.1-ThreadExample1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The first thread that is executed in a process is called the main thread
+            //The threads created using the Thread class are children threads
+            //When running the program, a thread is created
+            Thread thread = Thread.CurrentThread;//Pointer to the current thread.
+            thread.Name = "Main Thread";
+            Console.WriteLine("The current thread is {0}", thread.Name);
+
+            Console.ReadLine();
+            //Using the CurrentThreadProperty of the Thread Class, you crate a thread 
+            //object that points at the currently running thread (the main thread
+            //that is automatically created when running the programming.
+            //The thread object is assigned the name Main Thread.
+            //The name of the main thread is then displayed. 
+        }
+    }
+}
---
+++ Week9.1-ThreadExample2/Week9.1-ThreadExample2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //To create a childthread,
+            //Step 1: create a delegate object, passing 
+            //a callback method as a parameter.
+            //The callback method contains the 
+            //code you want executed when the child runs
+
+            //Step 2: A new thread object is created and the 
+            // deligate object is used to intialize the thread object
+
+            //Step 3: The start method on the thread object causes the thread to run
+
+
+
+        }
+
+
+    }
+}
